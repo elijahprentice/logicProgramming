2839aa1d1415a6de78ca60cfd4e7ac0474539d10
elijahprentice <prentee1213@gmail.com>
2020-02-09 15:56:42 -0500

    look into nested list (tail of list is lost)

--- csci350.pl
+++ csci350.pl
@@ -4,6 +4,7 @@ from-list(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-simple(L, N) :- is_list(L), from-list(0,L,N).
 
 from-list2(X, [Y|Z], W) :- number(Y), from-list2(X+Y,Z,W).
-from-list2(X, [Y|Z], W) :- not(number(Y)), from-list2(X,Z,W).
+from-list2(X, [Y|Z], W) :- is_list(Y), is_list(Z), from-list(X,Y,W).
+from-list2(X, [Y|Z], W) :- not(number(Y)), not(is_list(Y)),from-list2(X,Z,W).
 from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-general(L, N) :- is_list(L), from-list2(0,L,N).
