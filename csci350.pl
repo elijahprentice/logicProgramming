72695fa80c6a0bedfa5a6e40c45ba38b31fc96c5
elijahprentice <prentee1213@gmail.com>
2020-02-09 16:38:04 -0500

    look into nested list (error)

--- csci350.pl
+++ csci350.pl
@@ -4,7 +4,11 @@ from-list(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-simple(L, N) :- is_list(L), from-list(0,L,N).
 
 from-list2(X, [Y|Z], W) :- number(Y), from-list2(X+Y,Z,W).
-from-list2(X, [Y|Z], W) :- is_list(Y), is_list(Z), from-list(X,Y,W).
+from-list2(X, [Y|Z], W) :- is_list(Y), from-list(X,Y,W,Z).
 from-list2(X, [Y|Z], W) :- not(number(Y)), not(is_list(Y)),from-list2(X,Z,W).
+from-list2(X, [Y|Z], W, V) :- number(Y), from-list2(X+Y,Z,W,V).
+from-list2(X, [Y|Z], W, V) :- is_list(Y), from-list2(X,Y,W,Z).
+from-list2(X, [Y|Z], W, V) :- not(number(Y)), not(is_list(Y)), from-list2(X,Z,W,V).
+from-list2(X, Y, Z, W) :- length(Y,0), from-list2(X,W,Z).
 from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-general(L, N) :- is_list(L), from-list2(0,L,N).
