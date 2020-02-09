0f95b3a6879cde54f1ac53a553058e3d7973c6eb
elijahprentice <prentee1213@gmail.com>
2020-02-09 15:43:56 -0500

    sum up general (start from simple)

--- csci350.pl
+++ csci350.pl
@@ -2,3 +2,8 @@ from-list(X, [Y|Z], W) :- number(Y), from-list(X+Y,Z,W).
 from-list(X, [Y|Z], W) :- not(number(Y)), from-list(X,Z,W).
 from-list(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-simple(L, N) :- is_list(L), from-list(0,L,N).
+
+from-list2(X, [Y|Z], W) :- number(Y), from-list2(X+Y,Z,W).
+from-list2(X, [Y|Z], W) :- not(number(Y)), from-list2(X,Z,W).
+from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
+sum-up-numbers-general(L, N) :- is_list(L), from-list2(0,L,N).
