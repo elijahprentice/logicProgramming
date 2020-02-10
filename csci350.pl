16d02cd43d2f0a71c7fae415561a06dd84e58ef6
elijahprentice <prentee1213@gmail.com>
2020-02-10 10:53:32 -0500

    pass Z as head of V, only pass Z when aboce 0

--- csci350.pl
+++ csci350.pl
@@ -4,10 +4,12 @@ from-list(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-simple(L, N) :- is_list(L), from-list(0,L,N).
 
 from-list2(X, [Y|Z], W, V) :- number(Y), from-list2(X+Y,Z,W,V).
-from-list2(X, [Y|Z], W, V) :- is_list(Y), from-list2(X,Y,W,Z).
+from-list2(X, [Y|Z], W, V) :- is_list(Y), length(Z,0), from-list2(X,Y,W,V).
+from-list2(X, [Y|Z], W, V) :- is_list(Y), not(length(Z,0)), from-list2(X,Y,W,[Z|V]).
 from-list2(X, [Y|Z], W, V) :- not(number(Y)), not(is_list(Y)), from-list2(X,Z,W,V).
 from-list2(X, [Y|Z], W) :- number(Y), from-list2(X+Y,Z,W).
-from-list2(X, [Y|Z], W) :- is_list(Y), from-list2(X,Y,W,Z).
+from-list2(X, [Y|Z], W) :- is_list(Y), length(Z,0), from-list2(X,Y,W).
+from-list2(X, [Y|Z], W) :- is_list(Y), not(length(Z,0)), from-list2(X,Y,W,Z).
 from-list2(X, [Y|Z], W) :- not(number(Y)), not(is_list(Y)), from-list2(X,Z,W).
 from-list2(X, Y, Z, W) :- length(Y,0), from-list2(X,W,Z).
 from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
