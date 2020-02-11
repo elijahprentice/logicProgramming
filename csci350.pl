518c9c55a4bad83bc7c292a1cf128f165976650b
elijahprentice <prentee1213@gmail.com>
2020-02-10 21:49:02 -0500

    do not check value of non-numbers

--- csci350.pl
+++ csci350.pl
@@ -14,3 +14,8 @@ from-list2(X, [Y|Z], W) :- not(number(Y)), not(is_list(Y)), from-list2(X,Z,W).
 from-list2(X, Y, Z, W) :- length(Y,0), from-list2(X,W,Z).
 from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-general(L, N) :- is_list(L), from-list2(0,L,N).
+
+pass-var(W, [X|Y], Z) :- check-min(W,Y,X,Z).
+check-min(W, Y, X, Z) :- not(number(X)), pass-var(W,Y,Z).
+check-min(X, Y, Z) :- not(length(X,0)).
+min-above-min(L1, L2, N) :- is_list(L1), is_list(L2), check-min(L1,L2,N).
