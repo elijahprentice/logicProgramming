eb78e91ec4ce2fd4935691f08ee6723999037ce4
elijahprentice <prentee1213@gmail.com>
2020-02-10 21:57:52 -0500

    default max for L2, check if min of L1 = N

--- csci350.pl
+++ csci350.pl
@@ -15,7 +15,8 @@ from-list2(X, Y, Z, W) :- length(Y,0), from-list2(X,W,Z).
 from-list2(X, Y, Z) :- length(Y,0), X =:= Z.
 sum-up-numbers-general(L, N) :- is_list(L), from-list2(0,L,N).
 
-pass-var(W, [X|Y], Z) :- check-min(W,Y,X,Z).
-check-min(W, Y, X, Z) :- not(number(X)), pass-var(W,Y,Z).
-check-min(X, Y, Z) :- not(length(X,0)).
-min-above-min(L1, L2, N) :- is_list(L1), is_list(L2), check-min(L1,L2,N).
+pass-var(W, [X|Y], Z, A) :- check-min(W,Y,X,Z,A).
+check-min(W, Y, X, Z, A) :- not(number(X)), pass-var(W,Y,Z,A).
+check-min(W, Y, X, Z, A) :- number(X), not(length(Y,0)), X < A, pass-var(W,Y,X).
+check-min(X, Y, Z) :- not(length(X,0)), X =:= Z.
+min-above-min(L1, L2, N) :- is_list(L1), is_list(L2), pass-var(L1,L2,N,'500').
