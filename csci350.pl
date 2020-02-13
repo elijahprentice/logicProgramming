b9a54de1f77b6a2a9eba5087f060fdd809437fff
elijahprentice <prentee1213@gmail.com>
2020-02-12 22:43:34 -0500

    prevent infinite loop through nested lists

--- csci350.pl
+++ csci350.pl
@@ -42,11 +42,11 @@ find-element([LH|LT], [NH|NT], X) :- not(number(LH)), not(number(NH)), LH = NH, 
 find-element([LH|LT], [NH|NT], X) :- number(LH), number(NH), LH =:= NH, not(length(LT,0)), not(length(NT,0)), find-element(LT,NT,X).
 find-element([LH|LT], [NH|NT], X) :- number(LH), number(NH), LH =:= NH, length(LT,0), not(length(NT,0)), find-element(X,NT,X).
 find-element([LH|LT], [NH|NT], X) :- number(LH), number(NH), LH =:= NH, length(NT,0).
-find-element([LH|LT], N, X) :- is_list(LH), not(length(LT,0)), find-nested(LH,N), find-element(LT,N,X).
-find-element([LH|LT], N, X) :- is_list(LH), length(LT,0), find-nested(LH,N), find-element(LT,N,X).
-find-nested([LH|LT], [NH|NT]) :- not(number(LH)), not(number(NH)), LH = NH, not(length(LT,0)), not(length(NT,0)), find-nested(LT,NT).
-find-nested([LH|LT], [NH|NT]) :- not(number(LH)), not(number(NH)), LH = NH, length(LT,0).
-find-nested([LH|LT], [NH|NT]) :- not(number(LH)), not(number(NH)), LH = NH, length(NT,0).
+find-element([LH|LT], N, X) :- is_list(LH), not(length(LT,0)), find-nested(LH,N,LT).
+find-element([LH|LT], N, X) :- is_list(LH), length(LT,0), find-nested(LH,N,LT).
+find-nested([LH|LT], [NH|NT], T) :- not(number(LH)), not(number(NH)), LH = NH, not(length(LT,0)), not(length(NT,0)), find-nested(LT,NT,T).
+find-nested([LH|LT], [NH|NT], T) :- not(number(LH)), not(number(NH)), LH = NH, length(LT,0), find-element(T,NT,T).
+find-nested([LH|LT], [NH|NT], T) :- not(number(LH)), not(number(NH)), LH = NH, length(NT,0).
 find-nested([LH|LT], [NH|NT]) :- number(LH), number(NH), LH =:= NH, not(length(LT,0)), not(length(NT,0)), find-nested(LT,NT).
 find-nested([LH|LT], [NH|NT]) :- number(LH), number(NH), LH =:= NH, length(LT,0).
 find-nested([LH|LT], [NH|NT]) :- number(LH), number(NH), LH =:= NH, length(NT,0).
